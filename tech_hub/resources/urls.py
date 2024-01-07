8e7f53403ecf88502393fe14085db7f4de8272dc
Zack Adams <imzackadams@protonmail.com>
2024-01-07 04:26:19 -0500

    delete old files

--- tech_hub/resources/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import book_list  # Import the book_list view
-
-urlpatterns = [
-    path('books/', book_list, name='book_list'),  # Map /books/ to the book_list view
-]
