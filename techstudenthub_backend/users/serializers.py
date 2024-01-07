5ff2a345668cfd273328ab7cd013355351f83070
Zack Adams <imzackadams@protonmail.com>
2024-01-07 07:56:13 -0500

    add users app

---
+++ techstudenthub_backend/users/serializers.py
@@ -0,0 +1,14 @@
+# users/serializers.py
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
