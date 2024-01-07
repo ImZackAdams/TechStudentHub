35d6bfa9d69acb494c9c8a9446682707a966743f
Zack Adams <imzackadams@protonmail.com>
2024-01-07 05:53:02 -0500

    nextjs setup

--- techstudenthub_backend/techstudenthub_backend/settings.py
+++ techstudenthub_backend/techstudenthub_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%g6fv+3ixk@h^t7rneyxdw*kt)f*@%=*b_bm8)i4ck9*kp5xyz'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resources',
 ]
 
 MIDDLEWARE = [
