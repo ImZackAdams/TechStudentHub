4fcc0d2ddf44c502f70573f5e9f2a4e4874c5257
Zack Adams <imzackadams@protonmail.com>
2024-01-14 05:24:18 -0500

    add waitlist model

--- techstudenthub_backend/users/models.py
+++ techstudenthub_backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Waitlist(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
