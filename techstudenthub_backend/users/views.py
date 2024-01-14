2c6524bf02d7736d39966625aad1eeba9eb9f762
Zack Adams <imzackadams@protonmail.com>
2024-01-14 05:25:18 -0500

    add waitlist views

--- techstudenthub_backend/users/views.py
+++ techstudenthub_backend/users/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from .models import Waitlist
+import json
 
-## users/views.py
-from rest_framework import generics
-from .serializers import UserSerializer
 
-
-class CreateUserView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+@require_http_methods(["POST"])
+def waitlist_signup(request):
+    try:
+        data = json.loads(request.body)
+        email = data['email']
+        Waitlist.objects.create(email=email)
+        return JsonResponse({'status': 'success', 'message': 'Email added to waitlist'})
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
